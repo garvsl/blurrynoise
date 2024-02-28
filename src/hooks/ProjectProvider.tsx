8e29b93254742ff181df91255561b6f64f9e1a5e
garvsl <garvsehgal7@gmail.com>
2024-02-28 15:37:40 -0500

    create project provider

---
+++ src/hooks/ProjectProvider.tsx
@@ -0,0 +1,26 @@
+import { useContext, createContext, useState, useEffect } from "react";
+
+const ProjectContext = createContext<any>(null);
+
+export const useAuth = () => useContext(ProjectContext);
+
+export default function ProjectProvider({ children }: any) {
+  const [projects, setProjects] = useState<any>([
+    {
+      id: 1,
+      name: "OgProject",
+      files: ["null"],
+    },
+  ]);
+
+  return (
+    <ProjectContext.Provider
+      value={{
+        projects,
+        setProjects,
+      }}
+    >
+      {children}
+    </ProjectContext.Provider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./routes/Home.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import {
   Route,
   RouterProvider,
@@ -10,19 +10,88 @@ import {
 } from "react-router-dom";
 import Project from "./routes/Project.tsx";
 import RootLayout from "./RootLayout.tsx";
+import ProjectProvider from "./hooks/ProjectProvider.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RootLayout />}>
+    <Route
+    // element={<RootLayout />}
+    >
       <Route index path="/" element={<Home />} />
       <Route path="/project/:id" element={<Project />} />
     </Route>
   )
 );
+
+const activeLabelStyles = {
+  transform: "scale(0.85) translateY(-24px)",
+};
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        fontFamily: "Inter, Roboto, sans-serif",
+      },
+    },
+  },
+  components: {
+    Form: {
+      variants: {
+        floating: {
+          container: {
+            _focusWithin: {
+              label: {
+                ...activeLabelStyles,
+              },
+            },
+            "input:not(:placeholder-shown) + label, .chakra-select__wrapper + label, textarea:not(:placeholder-shown) ~ label":
+              {
+                ...activeLabelStyles,
+              },
+            label: {
+              top: 0,
+              left: 0,
+              zIndex: 2,
+              position: "absolute",
+              backgroundColor: "white",
+              pointerEvents: "none",
+              mx: 3,
+              px: 1,
+              my: 2,
+              transformOrigin: "left top",
+            },
+          },
+        },
+      },
+    },
+    Toast: {
+      baseStyle: {
+        fontFamily: "Inter, sans-serif",
+        ".chakra-alert__title": {
+          fontWeight: "900",
+        },
+      },
+    },
+    Heading: {
+      baseStyle: {
+        fontFamily: "Inter, sans-serif",
+      },
+    },
+    Text: {
+      baseStyle: {
+        fontFamily: "Inter, sans-serif",
+      },
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
+    <ChakraProvider theme={theme}>
+      <ProjectProvider>
+        <RouterProvider router={router} />
+      </ProjectProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
