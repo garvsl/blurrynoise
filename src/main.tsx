1afea010154dbfa7a121d1a01ce1941e5cf82049
garvsl <garvsehgal7@gmail.com>
2024-02-28 14:38:25 -0500

    setup routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children: [
+    //   {
+    //     path: "dashboard",
+    //     element: <Dashboard />,
+    //   },
+    //   {
+    //     path: "about",
+    //     element: <About />,
+    //   },
+    // ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
+      {/*<App />*/}
     </ChakraProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
