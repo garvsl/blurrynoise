5ef07f78aed1905a85437258433da4018000f4c6
garvsl <garvsehgal7@gmail.com>
2024-03-03 21:44:57 -0500

    add bootleg skeelton

--- src/components/Project/File.tsx
+++ src/components/Project/File.tsx
@@ -15,7 +15,7 @@ export default function File({ color, audio }: any) {
   const waveSurferRef: any = useRef();
   const cardRef: any = useRef();
   const effectCalled = useRef(false);
-  const [duration, setDuration] = useState<any>(0);
+  const [duration, setDuration] = useState<any>();
 
   const waveSurfer: any = (ref: any) => ({
     container: ref,
@@ -39,45 +39,54 @@ export default function File({ color, audio }: any) {
       waveSurferRef.current.on("ready", () => {
         setDuration(`${Math.round(waveSurferRef.current.getDuration())}s`);
       });
-      setLoading(false);
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
       effectCalled.current = true;
     }
   }, [audio]);
 
   return (
-    // <Skeleton isLoaded={!loading}>
-    <Card
-      paddingBottom={"5px"}
-      padding={"5px"}
-      ref={cardRef}
-      bg={`${color}.300`}
-      size={"sm"}
-    >
-      <CardHeader>
-        <Flex alignItems={"flex-end"}>
-          <Text
-            color={"white"}
-            fontSize={"14px"}
-            marginLeft={"-5px"}
-            paddingRight={"15px"}
-            letterSpacing={"-0.5px"}
-            isTruncated={true}
-            fontWeight={"500px"}
-          >
-            {audio.name}
-          </Text>
-          <Spacer />
-          <Text
-            paddingLeft={"15x"}
-            fontSize={"14px"}
-            letterSpacing={"-0.5px"}
-            color={"gray.200"}
-          >
-            {duration}
-          </Text>
-        </Flex>
-      </CardHeader>
-    </Card>
+    <>
+      <Skeleton
+        borderRadius={"md"}
+        height={"70px"}
+        display={loading ? "block" : "none"}
+      />
+      <Card
+        paddingBottom={"5px"}
+        padding={"5px"}
+        display={!loading ? "block" : "none"}
+        ref={cardRef}
+        bg={`${color}.300`}
+        size={"sm"}
+      >
+        <CardHeader>
+          <Flex alignItems={"flex-end"}>
+            <Text
+              color={"white"}
+              fontSize={"14px"}
+              marginLeft={"-5px"}
+              paddingRight={"15px"}
+              letterSpacing={"-0.5px"}
+              isTruncated={true}
+              fontWeight={"500px"}
+            >
+              {audio.name}
+            </Text>
+            <Spacer />
+            <Text
+              paddingLeft={"15x"}
+              fontSize={"14px"}
+              letterSpacing={"-0.5px"}
+              color={"gray.200"}
+            >
+              {duration}
+            </Text>
+          </Flex>
+        </CardHeader>
+      </Card>
+    </>
     // </Skeleton>
   );
 }
